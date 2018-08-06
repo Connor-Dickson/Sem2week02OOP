94eb1029d60a9fff77e29331210eee07e8624d87
Connor-Dickson <102051563@student.swin.edu.au>
2018-08-06 14:50:13 +1000

    Initial Commit

---
+++ Week 2/Week 2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 1;
+            int sum = 0;
+            int primeSum = 0;
+            for (int i = 0; i <= 7;)
+            {
+                sum = num1 + num2;
+                num1 = num2;
+                num2 = sum;
+                if (sum != 0 && sum != 1)
+                {
+                    for (int divisor = 2; divisor < 10; divisor++)
+                    {
+                        if (sum % divisor != 0)
+                        {
+                            primeSum += sum;
+                            i++;
+                        }
+                        
+                    }
+                }
+            }
+               
+            Console.WriteLine(primeSum);
+            Console.ReadLine();
+        }
+    }
+}
