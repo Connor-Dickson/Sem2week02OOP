76548744d3e699319180e7dba6e7aaae7fb3e493
Connor-Dickson <102051563@student.swin.edu.au>
2018-08-06 16:30:06 +1000

    End of Class Commit

---
+++ CIV-UnitTesting-Exercise-master/UnitTesting/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CIV_UnitTesting_Exercises;
+
+namespace UnitTesting
+{
+    [TestFixture]
+    public class Fibonaccitests
+    {
+        [TestCase]
+        public void When_FibocacciRec_Expect_FibonacciNumber()
+        {
+            RecursiveCalculations.FibocacciRec(1, 1, 6);
+            Assert.AreEqual()
+        }
+    } 
+}
