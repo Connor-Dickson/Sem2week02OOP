60cd58c082ee4ee606f44e1fbcb1fc277113bf26
CONNOR DICKSON <102051563@student.swin.edu.au>
2018-10-15 16:38:35 +1100

    End of Class Commit

---
+++ API/API/Controllers/ContactController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Contact")]
+    public class ContactController : Controller
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody] ContactPostRequestModel model)
+        {
+            return Ok(new
+            {
+                DateTime = DateTime.Now,
+                Model = model
+            });
+        }
+
+        public class ContactPostRequestModel
+        {
+            public string Name { get; set; }
+            public string EmailAddress { get; set; }
+            public string Message { get; set; }
+
+        }
+    }
+}
---
+++ API/ConsoleAppAPI/Program.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Task.Run(TaskAsync).Wait();
+
+           
+        }
+
+        static async Task TaskAsync()
+        {
+            Console.WriteLine("Enter Name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter Email");
+            string email = Console.ReadLine();
+            Console.WriteLine("Enter Message");
+            string message = Console.ReadLine();
+            HttpClient client = new HttpClient();
+            var StringContent = new StringContent(JsonConvert.SerializeObject(new Details(name, email, message)), System.Text.Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PostAsync("http://localhost:51964/api/Contact", StringContent);
+            var result = await response.Content.ReadAsStringAsync();
+        }
+
+        public class Details
+        {
+            public string Name { get; set; }
+            public string EmailAddress { get; set; }
+            public string Message { get; set; }
+
+            public Details(string name, string email, string message)
+            {
+                this.Name = name;
+                this.EmailAddress = email;
+                this.Message = message;
+            }
+        }
+    }
+}
